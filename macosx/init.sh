2add61642a96ead4372f72166d495eadffb18e4c
Evo Stamatov <aviolito@gmail.com>
2015-06-29 16:21:00 +1000

    add keyboard repeat rate change

--- macosx/init.sh
+++ macosx/init.sh
@@ -9,4 +9,8 @@ fi
 # Disable accented characters when long-pressing a key
 defaults write -g ApplePressAndHoldEnabled -bool false
 
+# Keyboard repeat and retreat rates
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+
 touch ./initialised
