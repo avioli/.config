de3ab83716097a3aa3240c9644edf3b869cbcace
Evo Stamatov <aviolito@gmail.com>
2015-07-09 12:42:51 +1000

    check for encrypted /mpw/sites.aes256 file and offer to decrypt it

--- fish/functions/mpw-init.fish
+++ fish/functions/mpw-init.fish
@@ -1,8 +1,16 @@
 function mpw-init -d "Initialises mpw defaults"
   # check if we've got the config dir
   if test -d ~/.config/mpw
-    set -gx MPW_SITENAMES ~/.config/mpw/sites
-    complete -c mpw -x -a "(cat $MPW_SITENAMES)"
+    set -q MPW_SITENAMES; or set -gx MPW_SITENAMES ~/.config/mpw/sites
+
+    if hash openssl 2>/dev/null
+      if test ! -f "$MPW_SITENAMES" -a -f "$MPW_SITENAMES.aes256"
+        echo "Found $MPW_SITENAMES.aes256. Would you like to decrypt it if you know the decryption password?"
+        openssl enc -aes256 -d -in "$MPW_SITENAMES.aes256" -out "$MPW_SITENAMES"; or rm "$MPW_SITENAMES"
+      end
+    end
+
+    test -f "$MPW_SITENAMES"; and complete -c mpw -x -a "(cat "$MPW_SITENAMES")"
   end
 end
 
