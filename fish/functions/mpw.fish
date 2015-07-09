2339b1103e0a1f88fbbf45e63b95a3abb5675baf
Evo Stamatov <aviolito@gmail.com>
2015-07-09 12:24:44 +1000

    fix subshell intricacies

--- fish/functions/mpw.fish
+++ fish/functions/mpw.fish
@@ -23,25 +23,20 @@ function mpw -d "Extends mpw to store the sitename for later recall"
   end
 
   set args (getopt u:t:c:V:v:C: $argv)
-  set args (fish -c "for el in $args; echo \$el; end")
+  # getopt would have mushed the args together with no hint if they were
+  # wrapped in quotes or not -- bad luck -- we don't care -- we only need the sitename
 
-  # rule out the regular mpw arguments
+  # make an array out of these args
+  set args (echo $args | awk 'BEGIN{FS=" ";OFS="\n"} {for (i=1;i<=NF;i++){printf("%s%s",sep,$i);sep=OFS};printf(OFS)}')
+
+  # rule out the recognised mpw arguments, if any
   set i 1
-  while true
-    switch $args[$i]
-      case "-u"
-      case "-t"
-      case "-c"
-      case "-V"
-      case "-v"
-      case "-C"
-      case "--"
-        break
-    end
+  while not test $args[$i] = '--'
     set i (math "$i + 1")
   end
 
-  # get the site name
+  # get the sitename
+  # in practice we only need $args[$i+1], but for error sake we'll get them all
   set pargs
   if math "$i <" (count $args) > /dev/null
     set pargs $args[(math "$i + 1")..-1]
@@ -49,19 +44,21 @@ function mpw -d "Extends mpw to store the sitename for later recall"
 
   set error 0
   if test (count $pargs) -eq 0
+    # no sitename heh?
     set error 1
   else if [ -z $pargs[1] ]
+    # a sitename shouldn't have spaces anyways :)
     set error 2
   end
 
-  if not [ -w $MPW_SITENAMES ]
+  if not [ -w "$MPW_SITENAMES" ]
     set error 3
     set errormessage "$MPW_SITENAMES is not writable"
   end
 
   if test $error -gt 0
     set_color red
-    if [ -z $errormessage ]
+    if [ -z "$errormessage" ]
       echo "Unable to store sitename"
     else
       echo $errormessage
