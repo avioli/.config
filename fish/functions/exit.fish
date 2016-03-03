34151046acce5b294d332b4645a1297c365660e1
Evo Stamatov <aviolito@gmail.com>
2016-03-03 11:36:49 +1100

    fix exit func

--- fish/functions/exit.fish
+++ fish/functions/exit.fish
@@ -1,16 +1,17 @@
 function exit --description "Exit current session, but warn if within a TMUX session"
+  status --is-interactive; or builtin exit $argv
   if set -q TMUX
     read -l -p "echo 'You are in TMUX session. Are you sure you want to exit or want to detach (Y/n/d) '" ASK
-    switch $ASK
+    switch (echo $ASK)
       case n N
         echo "OK"
       case d D
         detach
       case '*'
-        builtin exit
+        builtin exit $argv
     end
   else
-    builtin exit
+    builtin exit $argv
   end
 end
 
