0d42f85275c55d0a933792b1734e7d774c88ab3c
Evo Stamatov <aviolito@gmail.com>
2016-04-09 22:01:30 +1000

    add brew packages

---
+++ brew/packages.sh
@@ -0,0 +1,35 @@
+brew install graphicsmagick
+brew install yank
+brew install woof
+brew install fluidsynth
+brew install rem
+brew install offline-imap
+#brew install imagemagick --with-ghostscript
+brew install imagemagick
+brew install tmux
+brew install hardlink-osx
+brew install caskroom/cask/brew-cask
+brew install vimr
+brew install caskroom/cask/vimr
+brew install mono
+brew install task
+brew install watchman flow
+brew install tree
+brew install go
+brew install --HEAD neovim
+brew install wget
+brew install peco
+brew install rar
+brew install tig
+brew install rethinkdb
+brew install mpw
+brew install git
+brew install wifi-password
+brew install mpg123
+brew install lynx
+brew install ffmpeg --with-ffplay --with-fontconfig --with-freetype --with-libbluray --with-libvidstab --with-libvorbis --with-theora --with-tools --with-x265
+brew install subliminal
+brew install macvim --env-std --override-system-vim
+brew install cmake
+brew install vim
+brew install ranger
